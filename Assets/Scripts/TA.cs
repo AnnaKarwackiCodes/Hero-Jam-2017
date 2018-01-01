ae9ad95dc57ab2f14371ba311738ec30a33d15a9
avk9238 <avk9238@rit.edu>
2018-01-01 17:25:31 -0500

    Animation changes

--- Assets/Scripts/TA.cs
+++ Assets/Scripts/TA.cs
@@ -8,11 +8,13 @@ public class TA : MonoBehaviour {
     private int lastJoy;
     private int collisionJoy;
     private Animator animator;
+    private bool isMoving;
 	// Use this for initialization
 	void Start () {
         speed = 5;
         collisionJoy = -1;
         animator = this.GetComponent<Animator>();
+        isMoving = false;
 	}
 	
 	// Update is called once per frame
@@ -22,47 +24,74 @@ public class TA : MonoBehaviour {
 
     void Movement()
     {
-        if(Input.GetAxis("Left Joy Right") == 1 && collisionJoy != 0 &&transform.position.x < 8.5)
+        Debug.Log(animator.GetInteger("Direction"));
+        if((Input.GetAxis("Left Joy Right") == 1 || Input.GetKey(KeyCode.RightArrow)) && collisionJoy != 0 )//&&transform.position.x < 8.5)
         {
-            animator.enabled = true;
+            //animator.enabled = true;
             lastJoy = 0;
             animator.SetInteger("Direction", 2);
             transform.Translate(Vector2.right * speed * Time.deltaTime);
+            isMoving = true;
         }
-        else if(Input.GetAxis("Left Joy Right") == -1 && collisionJoy != 1 && transform.position.x > -9)
+        if((Input.GetAxis("Left Joy Right") == -1 || Input.GetKey(KeyCode.LeftArrow)) && collisionJoy != 1 )//&& transform.position.x > -9)
         {
-            animator.enabled = true;
+            //animator.enabled = true;
             lastJoy = 1;
             animator.SetInteger("Direction", 3);
             transform.Translate(Vector2.left * speed * Time.deltaTime);
+            isMoving = true;
         }
-        else if (Input.GetAxis("Left Joy Up") == 1 && collisionJoy != 2 && transform.position.y > -.5)
+        if ((Input.GetAxis("Left Joy Up") == 1 || Input.GetKey(KeyCode.DownArrow)) && collisionJoy != 2 )//&& transform.position.y > -.5)
         {
-            animator.enabled = true;
+            //animator.enabled = true;
             lastJoy = 2;
             animator.SetInteger("Direction", 1);
             transform.Translate(Vector2.down * speed * Time.deltaTime);
+            isMoving = true;
         }
-        else if (Input.GetAxis("Left Joy Up") == -1 && collisionJoy != 3 && transform.position.y < 7)
+        if ((Input.GetAxis("Left Joy Up") == -1 || Input.GetKey(KeyCode.UpArrow)) && collisionJoy != 3 )//&& transform.position.y < 7)
         {
-            animator.enabled = true;
+            //animator.enabled = true;
             lastJoy = 3;
             animator.SetInteger("Direction", 0);
             transform.Translate(Vector2.up * speed * Time.deltaTime);
+            isMoving = true;
         }
-        else
+        if((Input.GetAxis("Left Joy Right") == 0 && Input.GetAxis("Left Joy Up") == 0) && (!Input.GetKey(KeyCode.UpArrow)&& !Input.GetKey(KeyCode.DownArrow)&& !Input.GetKey(KeyCode.LeftArrow)&& !Input.GetKey(KeyCode.RightArrow)))
         {
-            animator.enabled = false;
+            isMoving = false;
+        }
+        if (!isMoving)
+        {
+            //animator.enabled = false;
+            
+            if (animator.GetInteger("Direction") == 0)
+            {
+                animator.SetInteger("Direction", 4);
+            }
+            else if (animator.GetInteger("Direction") == 1)
+            {
+                animator.SetInteger("Direction", 5);
+            }
+            else if (animator.GetInteger("Direction") == 2)
+            {
+                animator.SetInteger("Direction", 6);
+            }
+            else if (animator.GetInteger("Direction") == 3)
+            {
+                animator.SetInteger("Direction", 7);
+            }
+
         }
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("collisde");
-        collisionJoy = lastJoy;
+        //Debug.Log("collisde");
+        //collisionJoy = lastJoy;
     }
     public void OnTriggerExit2D(Collider2D collision)
     {
-        collisionJoy = -1;
+        //collisionJoy = -1;
     }
 
 }
